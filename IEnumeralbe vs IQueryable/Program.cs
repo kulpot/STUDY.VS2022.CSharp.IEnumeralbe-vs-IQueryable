9aa36d5e292788f04aee956b1ee1786c9165afc0
Kulpot <kulpotwahu@gmail.com>
2023-08-19 17:28:18 +0800

    Add project files.

---
+++ IEnumeralbe vs IQueryable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+//ref link:https://www.youtube.com/watch?v=7ssbfLdQGyg&list=PLRwVmtr-pp06SlwcsqhreZ2byuozdnPlg&index=8
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        int[] ints = new[] { 1, 9, 2, 7, 4, 3, 6, 5, };
+        IEnumerable<int> result =
+            //ints.Where(i => i < 5);
+            Enumerable.Where(ints, i => i < 5);
+
+        foreach (int i in result)
+            Console.WriteLine(i);
+
+    }
+}
